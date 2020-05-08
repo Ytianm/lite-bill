4fc45651898e2047f724aa7cd4b5e2f4947aee22
Ytianm <707764205@qq.com>
2020-05-09 01:12:36 +0800

    update

--- utils.js
+++ utils.js
@@ -1,3 +1,59 @@
+function ajax(options) {
+  const method = options.method || 'GET'
+  const async = options.async || true
+  let url = options.url
+  let data = options.data
+
+  let xhr
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest()
+  } else {
+    xhr = new ActiveXObject('Microsoft.XMLHttp')
+  }
+
+  xhr.open(method, url)
+
+  return new Promise((resolve, reject) => {
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
+          resolve && resolve(xhr.responseText)
+        } else {
+          reject && reject()
+        }
+      }
+    }
+
+    let dataArr = []
+    let paramStr
+    if (data instanceof Object) {
+      for (let key in data) {
+        dataArr.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      }
+      paramStr = dataArr.join('&')
+    }
+
+    if (method === 'GET') {
+      let index = url.indexOf('?')
+      if (index === -1) {
+        url += '?'
+      } else {
+        url += '&'
+      }
+      url += paramStr
+    }
+
+    xhr.open(method, url, async)
+
+    if (method === 'GET') {
+      xhr.send(null)
+    } else if (method === 'POST') {
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+      xhr.send(paramStr)
+    }
+  })
+}
+
 // csv字符串转换成对象数组
 function csvToObject(csvString) {
   let csvArr = csvString.split(/\n/g)
